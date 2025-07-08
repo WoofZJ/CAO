232894c7b4495e3be58c5420587be420a9b416e7
WoofZJ <zjwoof@gmail.com>
2025-07-08 18:01:44 +0800

    feat(client): update `VisitService` to include caching
    在 `VisitService` 中使用缓存避免短时间多次重复统计访问数据

--- CAO.Client/Services/VisitService.cs
+++ CAO.Client/Services/VisitService.cs
@@ -1,19 +1,28 @@
 using CAO.Shared.Dtos;
 using Microsoft.JSInterop;
+using Microsoft.Extensions.Caching.Memory;
 
 namespace CAO.Client.Services;
 
-public class VisitService(ApiService apiService, IJSRuntime jsRuntime, StorageService storageService)
+public class VisitService(
+    ApiService apiService, IJSRuntime jsRuntime,
+    StorageService storageService, IMemoryCache cache)
 {
     private readonly ApiService _apiService = apiService;
     private readonly IJSRuntime _jsRuntime = jsRuntime;
     private readonly StorageService _storageService = storageService;
+    private readonly IMemoryCache _cache = cache;
 
     public async Task<VisitGetResponse?> RecordVisitAsync()
     {
+        var path = await _jsRuntime.InvokeAsync<string>("eval", "location.pathname");
+        if (_cache.TryGetValue($"visit-{path}", out bool _))
+        {
+            return await GetVisitAsync();
+        }
+
         var userAgent = await _jsRuntime.InvokeAsync<string>("eval", "navigator.userAgent");
         var origin = await _jsRuntime.InvokeAsync<string>("eval", "location.origin");
-        var path = await _jsRuntime.InvokeAsync<string>("eval", "location.pathname");
         var query = await _jsRuntime.InvokeAsync<string>("eval", "location.search");
         var referer = await _jsRuntime.InvokeAsync<string>("eval", "document.referrer");
 
@@ -28,13 +37,18 @@ public class VisitService(ApiService apiService, IJSRuntime jsRuntime, StorageSe
             await _storageService.GetOrCreateSessionIdAsync()
         );
 
-        return await _apiService.PostAsync<VisitRecordRequest, VisitGetResponse>(
+        var result = await _apiService.PostAsync<VisitRecordRequest, VisitGetResponse>(
             "visit/record", request);
+        if (result is not null)
+        {
+            _cache.Set($"visit-{path}", true, TimeSpan.FromMinutes(15));
+        }
+        return result;
     }
 
     public async Task<VisitGetResponse?> GetVisitAsync()
     {
-        string path = await _jsRuntime.InvokeAsync<string>("location.pathname");
+        string path = await _jsRuntime.InvokeAsync<string>("eval", "location.pathname");
         VisitGetRequest request = new(path);
         return await _apiService.PostAsync<VisitGetRequest, VisitGetResponse>(
             "visit/get", request);
